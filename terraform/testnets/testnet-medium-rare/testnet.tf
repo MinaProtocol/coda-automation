5da4d5672597cf76a34484af66d98b3e96a98cee
Joel Krauska <jkrauska@gmail.com>
2019-08-13 13:38:57 -0700

    new ver

--- terraform/testnets/testnet-medium-rare/testnet.tf
+++ terraform/testnets/testnet-medium-rare/testnet.tf
@@ -2,7 +2,7 @@ locals {
   netname      = "medium-rare"
   aws_key_name = "testnet"
   coda_repo    = "release"
-  coda_version = "0.0.2-beta.2-ce3da112"
+  coda_version = "0.0.3-beta-9def15a5"
 }
 
 terraform {
