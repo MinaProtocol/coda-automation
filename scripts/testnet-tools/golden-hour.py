eed96a1cc5e33ec7e103d9ec5d5970acc2fa8b3c
Joel Krauska <jkrauska@gmail.com>
2019-09-12 15:40:37 -0700

    lots of keys, pep8

--- scripts/testnet-tools/golden-hour.py
+++ scripts/testnet-tools/golden-hour.py
@@ -7,13 +7,13 @@ from collections import Counter
 import pubkey_to_discord
 
 starttime = datetime(2019, 9, 11, 9, 0)
-endtime   = datetime(2019, 9, 11, 10, 0)
+endtime = datetime(2019, 9, 11, 10, 0)
 
 coda = CodaClient.Client(graphql_host="localhost", graphql_port="8304")
 blocks = coda.get_blocks()
 
-block_transactions={}
-block_provers={}
+block_transactions = {}
+block_provers = {}
 
 all_transactions = Counter()
 all_provers = Counter()
@@ -38,12 +38,14 @@ for block in blocks['blocks']['nodes']:
         block_provers[timestamp] = provers
         all_provers += provers
 
+
 def print_key_counter(mycounter, label='UNKNOWN'):
-    print(label,':\t', sum(mycounter.values()))
+    print(label, ':\t', sum(mycounter.values()))
     for (key, count) in mycounter.most_common():
         if key in pubkey_to_discord.keymap:
             key = pubkey_to_discord.keymap[key]
-        print("\t",count,"\t",key[:20])
+        print("\t", count, "\t", key[:20])
+
 
 timestamps = list(block_transactions.keys())
 timestamps.sort()
