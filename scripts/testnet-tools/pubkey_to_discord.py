ce3047967c124af061a9ea1a7c7d7f05f996864c
GregMas <55665209+GregMas@users.noreply.github.com>
2019-09-22 20:09:25 +0300

    Fillet Mignon: Adding Key for GregMas#3358

--- scripts/testnet-tools/pubkey_to_discord.py
+++ scripts/testnet-tools/pubkey_to_discord.py
@@ -13,6 +13,7 @@ FILET_MIGNON_STAKING_CHALLENGE = {
     'tNciaKqn2vMY7JA66AHEP1bupBcsnPYeFM58SorCWL78pGoVKrM9r7j968a9BaqmEqH2hx8UCCURy8op27HFE9auLfesTbu4u7MmRQmsGVmWhMfXSqpTQpmkcjzW7hd4Bc44G32jv2SLLS': 'Matt Harrop / Figment Network',
     'tNciBBcEcajd4AFCAiSLWvwg9X3fCUT57X5h9UhVuDhh3UrZ26nxCeVEzjFCN6uVhy2CFSqUjWqGsZ2yQno64nqE6SWPbqVr1WX2JiBQ23mBCmtkViPxCQUdq2EJZPyMDx6tWbazW2xgYo': 'garethtdavies#4963'
     'tNciVRxhXhJq78LFxg8acp7VwVSXtJMsQgyyASr9q9vXMcJ4whwawrtz1TZyH8TXHyy68cPatDR5xRn5G2o7cm8kWYMbooPaTVpte2vYsd7pqFDAJ6QAYTD8Ro1FPmzQdvCZHdhdMfdZqi': 'novy',
+    'tNci66YXVbXh5bMcyvZ6BhAb7YVeFhQg1a2VdXY8EKuCHQ7jYqFZYjpF7QEfzW3CZytYTtXKgF9eV6fH4SpUanFboMZiz4BAtCoAx2f6DXjofruZTikvirpN8mNTVoyiNVVbuowLfN5Dh3': 'GregMas#3358',
 }
 
 # For Community Members
