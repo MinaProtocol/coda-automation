c83e4710f9befa2deadd1d4b4487cdb3cc10aadb
Joel Krauska <jkrauska@gmail.com>
2020-01-06 15:40:01 -0800

    versioned start scripts

---
+++ ansible/coda_CLI_OPTIONS_0196a7b7c314514eea7f6ff6abe37d27.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Start scripting for coda daemon
+# Changelog:
+# gossip libp2p
+
+# Common CLI options for all roles
+CLI_COMMON="\
+ -background \
+ -tracing -log-json -log-level Trace \
+ -client-port ${CODA_RPCPORT} \
+ -external-port ${CODA_EXTPORT} \
+ -config-directory /home/admin/test-coda \
+ -external-ip ${EXTERNAL_IP} \
+ -rest-port ${CODA_QLPORT} \
+ -metrics-port ${CODA_METRICS_PORT} "
+
+# Main start
+case $CODA_ROLE in
+"seed")
+    CLI_ROLE="${CODA_SEEDLIST} "
+    ;;
+"seedjoiner")
+    CLI_ROLE="${CODA_SEEDLIST} "
+    ;;
+"snarkcoordinator")
+    export OMP_NUM_THREADS=4
+    CLI_ROLE="${CODA_SEEDLIST} \
+    -run-snark-worker ${PUBLIC_KEY} \
+    -snark-worker-fee 2 \
+    -work-selection seq "
+    ;;
+"blockproducer")
+    CLI_ROLE="${CODA_SEEDLIST} \
+    -propose-key /home/admin/wallet-keys/proposerkey "
+    ;;
+"archive")
+    CLI_ROLE="${CODA_SEEDLIST} \
+    -archive "
+    ;;
+*)
+    echo "Uknown CODA_ROLE ${CODA_ROLE}"
+    exit
+esac
+
+DISCOVERY_KEYFILE="/home/admin/discovery_keys/discovery_key"
+if [[ -f $DISCOVERY_KEYFILE ]]; then
+    echo 'Using stored libp2p discovery key'
+    CLI_ROLE+=" -discovery-keypair discovery_keys/discovery_key"
+    export CODA_LIBP2P_PASS='testnet'
+fi
+
+echo "Starting coda ${CODA_ROLE}"
+CMD="coda daemon ${CLI_COMMON} ${CLI_ROLE}"
+echo "Running: ${CMD}"
+
+if [ "$DRYRUN" = false ]; then
+    CODA_PRIVKEY_PASS="{{ privkey_pass }}"  ${CMD}
+else
+    echo "NO-OP: DryRun"
+fi
