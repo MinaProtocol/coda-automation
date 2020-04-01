72706599b40fba7ce238cc1ec7c394e61b8fa2b0
Brandon Kase <bkase@o1labs.org>
2020-04-01 18:09:06 -0400

    Initial testnets for release

---
+++ terraform/testnets/three-point-two/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "~> 0.12.0"
+  backend "s3" {
+    key     = "terraform-hangry-lobster.tfstate"
+    encrypt = true
+    region  = "us-west-2"
+    bucket  = "o1labs-terraform-state"
+    acl     = "bucket-owner-full-control"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+locals {
+  testnet_name = "hangry-lobster"
+  coda_image   = var.coda_image
+  coda_agent_image = var.coda_agent_image
+}
+
+variable "coda_image" {
+  type = string
+  default = "codaprotocol/coda-daemon:0.0.13-beta-testnet-hangry-lobster-b3b9d23"
+}
+
+variable "coda_agent_image" {
+  type = string
+  default = "codaprotocol/coda-user-agent:0.1.3-gerald"
+}
---
+++ terraform/testnets/three-point-two/us-east1.tf
@@ -0,0 +1,54 @@
+provider "google" {
+  alias   = "google-us-east1"
+  project = "o1labs-192920"
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
+
+
+module "testnet_east" {
+  providers = {
+    google = google.google-us-east1
+  }
+  source                = "../../modules/kubernetes/testnet"
+  cluster_name          = "coda-infra-east"
+  cluster_region        = "us-east1"
+  testnet_name          = local.testnet_name
+  coda_image            = local.coda_image
+  coda_agent_image      = local.coda_agent_image
+
+  seed_zone = "us-east1-b"
+  seed_region = "us-east1"
+
+  num_whale_block_producers = 5
+  num_fish_block_producers = 200
+  block_producer_key_pass = "naughty blue worm"
+  block_producer_starting_host_port = 10001
+
+  snark_worker_replicas = 1
+  snark_worker_fee      = 2
+  snark_worker_public_key = "4vsRCVQZ41uqXfVVfkBNUuNNS7PgSJGdMDNAyKGDdU1WkdxxyxQ7oMdFcjDRf45fiGKkdYKkLPBrE1KnxmyBuvaTW97A5C8XjNSiJmvo9oHa4AwyVsZ3ACaspgQ3EyxQXk6uujaxzvQhbLDx"
+  snark_worker_host_port = 10400
+}
+
+# Seed DNS
+data "aws_route53_zone" "selected" {
+  name = "o1test.net."
+}
+
+resource "aws_route53_record" "seed_one" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "seed-one.${local.testnet_name}.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  ttl     = "300"
+  records = [module.testnet_east.seed_one_ip]
+}
+
+resource "aws_route53_record" "seed_two" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "seed-two.${local.testnet_name}.${data.aws_route53_zone.selected.name}"
+  type    = "A"
+  ttl     = "300"
+  records = [module.testnet_east.seed_two_ip]
+}
+
