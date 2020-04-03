d4809df0da84770920dc5506b371bc6c1ccc616c
Nathan Holland <nholland94@gmail.com>
2020-04-02 18:31:53 -0700

    Pin coda daemon image

--- terraform/testnets/bugspray/main.tf
+++ terraform/testnets/bugspray/main.tf
@@ -24,7 +24,7 @@ locals {
 
 variable "coda_image" {
   type = string
-  default = "codaprotocol/coda-daemon:0.0.12-beta-release-0.0.13-beta-cf92c69"
+  default = "codaprotocol/coda-daemon:0.0.12-beta-release-0.0.13-beta-abd7562"
 }
 
 variable "coda_agent_image" {
