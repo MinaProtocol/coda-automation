317ad709d9d02ae84661e94046dc9f226eb862d5
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-05-11 16:42:00 -0400

    refactor variable names and add agent token to k8s secret store

--- terraform/modules/kubernetes/buildkite-agent/kubernetes.tf
+++ terraform/modules/kubernetes/buildkite-agent/kubernetes.tf
@@ -22,8 +22,21 @@ resource "kubernetes_secret" "google_application_credentials" {
   metadata {
     name = "google-application-credentials"
     namespace = kubernetes_namespace.cluster_namespace.metadata[0].name
+    annotations = {
+      "kubernetes.io/service-account.name" = "google-application-credentials"
+    }
   }
   data = {
-    "credentialsjson" = base64decode(google_service_account_key.buildkite_svc_key.private_key)
+    "credentials_json" = base64decode(google_service_account_key.buildkite_svc_key.private_key)
+  }
+}
+
+resource "kubernetes_secret" "buildkite_agent_token" {
+  metadata {
+    name = "buildkite-agent-token"
+    namespace = kubernetes_namespace.cluster_namespace.metadata[0].name
+  }
+  data = {
+    "agent_token" = base64decode(var.agent_token)
   }
 }
