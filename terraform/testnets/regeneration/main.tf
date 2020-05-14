51a0edf27a4b79968dfe7a94147c9ae544ed6b68
Conner Swann <conner@o1labs.org>
2020-05-13 22:52:22 -0700

    save

--- terraform/testnets/regeneration/main.tf
+++ terraform/testnets/regeneration/main.tf
@@ -19,5 +19,5 @@ locals {
 }
 variable "coda_image" {
   type = string
-  default = "codaprotocol/coda-daemon:0.0.12-beta-new-ledger-c502e01"
+  default = "codaprotocol/coda-daemon:0.0.12-beta-new-ledger-b59cbcc"
 }
