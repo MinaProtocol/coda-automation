81b5cc8de5ed824dc619b929450f9fa056bd4e37
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-06-24 14:03:06 -0400

    update AWS policy setup

--- terraform/modules/kubernetes/buildkite-agent/src/aws.tf
+++ terraform/modules/kubernetes/buildkite-agent/src/aws.tf
@@ -3,29 +3,25 @@ provider "aws" {
   region = "us-west-2"
 }
 
+data "aws_iam_policy_document" "buildkite_aws_policy" {
+  statement {
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+
+    # TODO: narrow to buildkite agent pipeline specific set of secrets
+    resources = [
+      "*",
+    ]
+  }
+}
+
 resource "aws_iam_role" "buildkite_agent" {
   name = "buildkite_agent"
 
   force_detach_policies = true
 
-  assume_role_policy = <<EOF
-{
-"Version" : "2020-06-24",
-  "Statement" : [
-    {
-      "Effect": "Allow",
-      "Principal": {<insert-proper-principla>},
-      "Action": "secretsmanager:GetSecretValue",
-      "Resource": "*",
-      "Condition": {
-        "ForAnyValue:StringEquals": {
-          "secretsmanager:VersionStage" : "AWSCURRENT"
-        }
-      }
-    }
-  ]
-}
-EOF
+  assume_role_policy = "${data.aws_iam_policy_document.buildkite_aws_policy.json}"
 
   tags = {
     cluster = var.cluster_name
