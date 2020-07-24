1f1b22422616a40170a601e3a4ce096f186da161
Izaak Meckler <ihmeckler@gmail.com>
2020-07-24 13:04:05 -0700

    terraform files for qanet

---
+++ terraform/testnets/new-snark-propagation-time/us-east1.tf
@@ -0,0 +1,34 @@
+provider "google" {
+  alias   = "google-us-east1"
+  project = "o1labs-192920"
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
+
+
+module "testnet_east" {
+  providers = {
+    google = google.google-us-east1
+  }
+  source                = "../../modules/kubernetes/testnet"
+  cluster_name          = "coda-infra-east"
+  cluster_region        = "us-east1"
+  testnet_name          = local.testnet_name
+  coda_image            = local.coda_image
+  genesis_timestamp     = timestamp()
+  ledger_config_location = local.ledger_config_location
+
+  seed_zone = "us-east1-b"
+  seed_region = "us-east1"
+
+  num_whale_block_producers = 5
+  num_fish_block_producers = 1
+  block_producer_key_pass = "naughty blue worm"
+  block_producer_starting_host_port = 10001
+
+  snark_worker_replicas = 1
+  snark_worker_fee      = 0.00000006         
+  snark_worker_public_key = "B62qp6vSpiLvCeYhqF4hx1s6nQiKEeJJyzh2aSuoPDnkPSSBaeyLtau"
+  snark_worker_host_port = 10400
+}
+
