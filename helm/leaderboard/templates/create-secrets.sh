456d85b505682346bf3b5ec1b424625b18861356
Martin Minkov <minkovlmartin@gmail.com>
2020-08-19 11:56:27 -0700

    refactored credentials to volume instead of env

--- helm/leaderboard/templates/create-secrets.sh
+++
@@ -1,2 +0,0 @@
-# TODO: Not sure where google_sheets_credentials.json will be stored. Will it need be accessed by a volume? 
-kubectl create secret generic credentials --from-file=google_sheets_credentials.json
