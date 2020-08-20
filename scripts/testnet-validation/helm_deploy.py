9425aee9e347799ca0526f1de37294af8af116c7
Conner Swann <conner@o1labs.org>
2020-08-20 13:56:18 -0700

    new helm deploy script

--- scripts/testnet-validation/helm_deploy.py
+++ scripts/testnet-validation/helm_deploy.py
@@ -8,12 +8,13 @@ import json
 import random
 import re
 import sys
+import yaml
 from pathlib import Path
 
 # This script is designed to fetch relevant parameters from a running testnet
-# and run a daemon locally that is correctly configured to connect to that testnet.
+# and run a Coda helm chart against that testnet.
 # Example Invocation:
-# python3 scripts/testnet-validation/local_daemon.py run --working-directory ~/Desktop/config --coda-docker-image codaprotocol/coda-daemon:0.0.12-beta-rosetta-dockerfile-aec5631
+# python3 scripts/testnet-validation/helm_deploy.py run --working-directory ~/Desktop/config --coda-docker-image codaprotocol/coda-daemon:0.0.12-beta-rosetta-dockerfile-aec5631
 
 SCRIPT_DIR = Path(__file__).parent.absolute()
 config.load_kube_config()
@@ -29,9 +30,8 @@ def cli(debug):
 def fetch_peers(namespace="default", v1=client.CoreV1Api()):
   # Load K8s config
   ret = v1.list_namespaced_pod(namespace=namespace)
-  random.shuffle(ret.items)
-  peers = ret.items[:3]
-
+  filtered_pods = filter(lambda pod: "block-producer" in pod.metadata.name, ret.items)
+  peers = random.sample(list(filtered_pods), 3)
   peer_addresses = []
   for i in peers:
     # Run coda client status and get output
@@ -43,6 +43,8 @@ def fetch_peers(namespace="default", v1=client.CoreV1Api()):
       sys.exit(1)
     if output:
       status_output = output.decode('utf8')
+    else:
+      raise Exception("No Status Output...")
 
     # grep for:
     #   IP Address
@@ -66,6 +68,27 @@ def fetch_peers(namespace="default", v1=client.CoreV1Api()):
       sys.exit(1)
   return peer_addresses
 
+def fetch_helm_values(namespace="default", release_name=None):
+  if not release_name:
+    raise Exception("Please pass a release name.")
+
+  # run helm get values <release_name> 
+  command = f"helm get values --namespace {namespace} {release_name}"
+  process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+  output, error = process.communicate()
+  if error:
+    print(error)
+    sys.exit(1)
+  if output:
+    values_raw = output.decode('utf8')
+    values = yaml.safe_load(values_raw)
+    return values
+  else:
+    raise Exception("No values retrieved, something is wrong!")
+
+    
+
+  
 
 def fetch_daemon_json(working_directory, namespace="default", v1=client.CoreV1Api()):
   config_name = "seed-node-daemon-config"
@@ -77,47 +100,55 @@ def fetch_daemon_json(working_directory, namespace="default", v1=client.CoreV1Ap
       outfile.write(config.data["daemon.json"])
       print(f"Wrote daemon.json to {outpath}")
 
+def helm_install(namespace, overrides_file, chart_path, release_name):
+  command = f"helm install --values {overrides_file} {release_name} {chart_path}"
+  process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+  output, error = process.communicate()
+  if error:
+    print(error)
+    sys.exit(1)
+  if output:
+    command_result = output.decode('utf8')
+    return command_result
+  else:
+    raise Exception("No Command Output... Something is wrong.")
 
-def run_docker(daemon_image, gossip_port, working_directory, peers):
-  command = f"""
-daemon \
-  -external-port {gossip_port} \
-  -generate-genesis-proof true \
-  -config-dir /root/.coda-config \
-  -config-file /tmp/daemon.json \
-  -peer {peers[0]} \
-  -peer {peers[2]} \
-  -peer {peers[1]}
-  """
-  print(command)
-  dClient = docker.from_env()
-  container = dClient.containers.run(
-    daemon_image,
-    # entrypoint="bash -c",
-    command=command,
-    ports={f"{gossip_port}/tcp": gossip_port},
-    volumes={working_directory.absolute(): {'bind': '/tmp', 'mode': 'rw'}},
-    detach=True
-  )
-  print(container.logs())
 
 @cli.command()
 @click.option("--namespace", default="regeneration", help="The name of the testnet you'd like to connect to.")
-@click.option("--gossip-port", default=10000, help="The port to expose on the Coda Daemon Docker Container.")
-@click.option("--docker-image", default="codaprotocol/coda-daemon:latest", help="The Coda Daemon Docker Image to use.")
+@click.option("--values-file", default=None, help="An Optional values.yaml, used to override chart defaults.")
+@click.option("--release-name", default=None, help="The name of the release to pull values from.")
 @click.option("--working-directory", default=".", help="The location to download temporary files to, namely the daemon.json")
-def run(namespace, gossip_port, docker_image, working_directory):
+def run(namespace, values_file, release_name, working_directory):
   if working_directory == ".":
     working_directory = SCRIPT_DIR
   else:
     working_directory = Path(working_directory)
 
+  if not release_name:
+    raise Exception("Please enter a release name when invoking this script.")
+
+  # Fetch Peers
   addresses = fetch_peers(namespace)
-  fetch_daemon_json(working_directory, namespace)
+  # Fetch Helm Release Values
+  values = fetch_helm_values(namespace, release_name)
+  # Parse out coda.runtimeGenesis and prep overrides
+  overrides = {
+    "coda":{
+      "runtimeConfig": values["coda"]["runtimeConfig"],
+      "seedPeers": addresses
+    }
+  }
+  # Write overrides to file
+  overrides_file = working_directory / "overrides.yaml"
+  with open(overrides_file, "w") as outfile:
+    yaml.dump(overrides, outfile)
 
-  run_docker(docker_image, gossip_port, working_directory, peers=addresses)
+  #run_docker(docker_image, gossip_port, working_directory, peers=addresses)
 
-
+  # `helm install` with merged values.yaml to specified namespace
+  chart_path = "/Users/connerswann/code/coda-automation/helm/archive-node"
+  output = helm_install(namespace, overrides_file.absolute(), chart_path, "test-archive")
 
 
 if __name__ == "__main__":
