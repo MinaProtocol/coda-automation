985b673eb9bfb176ff13f80ade585a14c199a3c8
Izaak Meckler <ihmeckler@gmail.com>
2020-09-05 18:05:28 -0700

    reduce snark workers from 128 to 64, reduce fish from 10 to 4, increase whales from 5 to 16

--- terraform/testnets/pickles/main.tf
+++ terraform/testnets/pickles/main.tf
@@ -20,14 +20,9 @@ provider "google" {
   zone    = "us-east1-b"
 }
 
-<<<<<<< HEAD
 locals {
   testnet_name = "pickles"
-<<<<<<< HEAD
   coda_image = "codaprotocol/coda-daemon:0.0.16-beta7-debug-adding-ledger-catchup-logs-049e329"
-=======
-  coda_image = "gcr.io/o1labs-192920/coda-daemon:0.0.16-beta7"
->>>>>>> test-public-testnet-deploy Update docker image tag for new chain and re-deploy
   seed_region = "us-east1"
   seed_zone = "us-east1-b"
   seed_discovery_keypairs = [
@@ -35,15 +30,12 @@ locals {
 ]
 }
 
-=======
->>>>>>> pickles
 module "testnet_east" {
   providers = { google = google.google-us-east1 }
   source    = "../../modules/kubernetes/testnet"
 
   cluster_name          = "coda-infra-east"
   cluster_region        = "us-east1"
-<<<<<<< HEAD
   testnet_name          = local.testnet_name
 
   coda_image            = local.coda_image
@@ -60,42 +52,12 @@ module "testnet_east" {
   seed_discovery_keypairs = [
  "CAESQNf7ldToowe604aFXdZ76GqW/XVlDmnXmBT+otorvIekBmBaDWu/6ZwYkZzqfr+3IrEh6FLbHQ3VSmubV9I9Kpc=,CAESIAZgWg1rv+mcGJGc6n6/tyKxIehS2x0N1Uprm1fSPSqX,12D3KooWAFFq2yEQFFzhU5dt64AWqawRuomG9hL8rSmm5vxhAsgr" 
 ]
-=======
-  testnet_name          = "pickles"
-
-  coda_image            = "codaprotocol/coda-daemon:0.0.15-beta-release-0.0.15-beta-7f75e63"
-  coda_agent_image      = "codaprotocol/coda-user-agent:0.1.5"
-  coda_bots_image       = "codaprotocol/coda-bots:0.0.13-beta-1"
-  coda_points_image     = "codaprotocol/coda-points-hack:32b.4"
-
-  coda_faucet_amount    = "10000000000"
-  coda_faucet_fee       = "100000000"
-
-  runtime_config = <<EOT
-    {
-      "daemon": {},
-      "genesis": { 
-        "genesis_state_timestamp": "${timestamp()}",
-        "k": 20, 
-        "delta": 3
-      },
-      "proof": {
-        "c": 8
-      },
-      "ledger": ${file("../../../scripts/genesis_ledger.json")}
-    }
-  EOT
-
-  seed_zone = "us-east1-b"
-  seed_region = "us-east1"
->>>>>>> pickles
 
   log_level              = "Trace"
   log_txn_pool_gossip    = true
   log_received_blocks    = true
 
   block_producer_key_pass = "naughty blue worm"
-<<<<<<< HEAD
   block_producer_starting_host_port = 10005
 
   block_producer_configs = concat(
@@ -116,43 +78,13 @@ module "testnet_east" {
         class                  = "whale"
         id                     = i + 1
         private_key_secret     = "online-whale-${i + 1}-key"
-=======
-  block_producer_starting_host_port = 10001
-
-  block_producer_configs = concat(
-    [
-      for i in range(5): {
-        name                   = "whale-block-producer-${i + 1}"
-        class                  = "whale"
-        id                     = i + 1
-        private_key_secret     = "online-whale-account-${i + 1}-key"
-        enable_gossip_flooding = false
-        run_with_user_agent    = false
-        run_with_bots          = false
-      }
-    ],
-    [
-      for i in range(400): {
-        name                   = "fish-block-producer-${i + 1}"
-        class                  = "fish"
-        id                     = i + 1
-        private_key_secret     = "online-fish-account-${i + 1}-key"
->>>>>>> pickles
         enable_gossip_flooding = false
         run_with_user_agent    = true
-        run_with_bots          = true
+        run_with_bots          = false
       }
     ]
   )
 
-<<<<<<< HEAD
-  snark_worker_replicas = 8
-  snark_worker_fee      = "10"
-  snark_worker_public_key = "B62qotoVypDR2w7yUVSxFmyhbcn2SqdSwoJDPo5YmPyha7MyXPY891G"
-  snark_worker_host_port = 10400
-}
-
-=======
   snark_worker_replicas = 128
   snark_worker_fee      = "0.025"
   snark_worker_public_key = "B62qk4nuKn2U5kb4dnZiUwXeRNtP1LncekdAKddnd1Ze8cWZnjWpmMU"
@@ -163,4 +95,3 @@ module "testnet_east" {
   agent_min_tx = "0.0015"
   agent_max_tx = "0.0015"
 }
->>>>>>> pickles
