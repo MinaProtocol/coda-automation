d21e67de6fe1ad38ab27c3a8e3969a32c6414fe9
Izaak Meckler <ihmeckler@gmail.com>
2020-09-16 11:12:50 -0700

    fix seed peer dns

--- terraform/modules/kubernetes/testnet/helm.tf
+++ terraform/modules/kubernetes/testnet/helm.tf
@@ -17,7 +17,7 @@ provider helm {
 
 locals {
   seed_peers = [
-    "/dns4/seed-one.${var.testnet_name}.o1test.net/tcp/10001/p2p/${split(",", var.seed_discovery_keypairs[0])[2]}"
+    "/dns4/seed-node.${var.testnet_name}/tcp/10001/p2p/${split(",", var.seed_discovery_keypairs[0])[2]}"
   ]
 
   coda_vars = {
