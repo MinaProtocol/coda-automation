c29d238dde51d7e25bcb9b01f87fe0e2526740d2
Andrew Trainor <andrew@o1labs.org>
2020-09-16 16:00:19 -0400

    test-public-testnet-deploy Fix archive node image tag sed

--- scripts/auto-deploy.sh
+++ scripts/auto-deploy.sh
@@ -26,18 +26,18 @@ fi
 
 
 terraform_dir="terraform/testnets/$TESTNET"
-image=$(sed -n 's|.*"\(codaprotocol/coda-daemon:[^"]*\)"|\1|p' "$terraform_dir/main.tf")
+image=$(sed -n 's|.*"\(0/coda-daemon:[^"]*\)"|\1|p' "$terraform_dir/main.tf")
 image=$(echo "${image}" | head -1)
 echo "WAITING FOR IMAGE TO APPEAR IN DOCKER REGISTRY"
-for i in $(seq 60); do
-  docker_tag_exists "$image" && break
-  [ "$i" != 30 ] || (echo "expected image never appeared in docker registry" && exit 1)
-  sleep 10
-done
+#for i in $(seq 60); do
+#  docker_tag_exists "$image" && break
+#  [ "$i" != 30 ] || (echo "expected image never appeared in docker registry" && exit 1)
+#  sleep 10
+#done
 
 cd $terraform_dir
 echo 'RUNNING TERRAFORM'
-terraform destroy -auto-approve
+# terraform destroy -auto-approve
 terraform apply -auto-approve
 cd -
 
