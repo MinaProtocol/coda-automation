c96d30b670062b7a61a949c60b27cb0941fd07f1
Andrew Trainor <andrew@o1labs.org>
2020-09-16 03:04:13 -0400

    test-public-testnet-deploy Remove second seed node

--- terraform/testnets/test-public-deploy/public-seeds.tf
+++ terraform/testnets/test-public-deploy/public-seeds.tf
@@ -27,21 +27,21 @@ module "seed_one" {
   seed_peers         = ""
 }
 
-module "seed_two" {
-  providers = { google = google.google-us-east1 }
-  source             = "../../modules/google-cloud/coda-seed-node"
-  coda_image         = local.coda_image
-  project_id         = "o1labs-192920"
-  subnetwork_project = "o1labs-192920"
-  subnetwork         = module.seed_network.subnet_link
-  network            = module.seed_network.network_link
-  instance_name      = "${local.testnet_name}-seed-two-${local.seed_region}"
-  zone               = local.seed_zone
-  region             = local.seed_region
-  client_email       = "1020762690228-compute@developer.gserviceaccount.com"
-  discovery_keypair  = local.seed_discovery_keypairs[1]
-  seed_peers         = "-peer /ip4/${module.seed_one.instance_external_ip}/tcp/10001/p2p/${split(",", module.seed_one.discovery_keypair)[2]}"
-}
+#module "seed_two" {
+#  providers = { google = google.google-us-east1 }
+#  source             = "../../modules/google-cloud/coda-seed-node"
+#  coda_image         = local.coda_image
+#  project_id         = "o1labs-192920"
+#  subnetwork_project = "o1labs-192920"
+#  subnetwork         = module.seed_network.subnet_link
+#  network            = module.seed_network.network_link
+#  instance_name      = "${local.testnet_name}-seed-two-${local.seed_region}"
+#  zone               = local.seed_zone
+#  region             = local.seed_region
+#  client_email       = "1020762690228-compute@developer.gserviceaccount.com"
+#  discovery_keypair  = local.seed_discovery_keypairs[1]
+#  seed_peers         = "-peer /ip4/${module.seed_one.instance_external_ip}/tcp/10001/p2p/${split(",", module.seed_one.discovery_keypair)[2]}"
+#}
 
 # Seed DNS
 data "aws_route53_zone" "selected" {
@@ -56,10 +56,10 @@ resource "aws_route53_record" "seed_one" {
   records = [module.seed_one.instance_external_ip]
 }
 
-resource "aws_route53_record" "seed_two" {
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "seed-two.${local.testnet_name}.${data.aws_route53_zone.selected.name}"
-  type    = "A"
-  ttl     = "300"
-  records = [module.seed_two.instance_external_ip]
-}
+#resource "aws_route53_record" "seed_two" {
+#  zone_id = data.aws_route53_zone.selected.zone_id
+#  name    = "seed-two.${local.testnet_name}.${data.aws_route53_zone.selected.name}"
+#  type    = "A"
+#  ttl     = "300"
+#  records = [module.seed_two.instance_external_ip]
+#}
