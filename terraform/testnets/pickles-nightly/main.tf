0d9da7b5d434995e9f21dbcbc70bf293616eec0e
Evan Shapiro <evanshapiro@MacBook-Pro-2.local>
2020-09-25 14:34:38 -0700

    public seeds

--- terraform/testnets/pickles-nightly/main.tf
+++ terraform/testnets/pickles-nightly/main.tf
@@ -20,15 +20,26 @@ provider "google" {
   zone    = "us-east1-b"
 }
 
+locals {
+  testnet_name = "pickles-nightly"
+  coda_image = "codaprotocol/coda-daemon:0.0.16-beta7-feature-mainnet-parameter-test-f501ee8"
+  seed_region = "us-east1"
+  seed_zone = "us-east1-b"
+  seed_discovery_keypairs = [
+  "CAESQBEHe2zCcQDHcSaeIydGggamzmTapdCS8SP0hb5FWvYhe9XEygmlUGV4zNu2P8zAIba4X84Gm4usQFLamjRywA8=,CAESIHvVxMoJpVBleMzbtj/MwCG2uF/OBpuLrEBS2po0csAP,12D3KooWJ9mNdbUXUpUNeMnejRumKzmQF15YeWwAPAhTAWB6dhiv",
+  "CAESQO+8qvMqTaQEX9uh4NnNoyOy4Xwv3U80jAsWweQ1J37AVgx7kgs4pPVSBzlP7NDANP1qvSvEPOTh2atbMMUO8EQ=,CAESIFYMe5ILOKT1Ugc5T+zQwDT9ar0rxDzk4dmrWzDFDvBE,12D3KooWFcGGeUmbmCNq51NBdGvCWjiyefdNZbDXADMK5CDwNRm5" ]
+}
+
+
 module "testnet_east" {
   providers = { google = google.google-us-east1 }
   source    = "../../modules/kubernetes/testnet"
 
   cluster_name          = "coda-infra-east"
   cluster_region        = "us-east1"
-  testnet_name          = "pickles-nightly"
+  testnet_name          = local.testnet_name
 
-  coda_image            = "codaprotocol/coda-daemon:0.0.16-beta7-feature-mainnet-parameter-test-f501ee8"
+  coda_image            = local.coda_image
   coda_agent_image      = "codaprotocol/coda-user-agent:0.1.5"
   coda_bots_image       = "codaprotocol/coda-bots:0.0.13-beta-1"
   coda_points_image     = "codaprotocol/coda-points-hack:32b.4"
@@ -39,7 +50,7 @@ module "testnet_east" {
   runtime_config = <<EOT
     {
       "daemon": {},
-      "genesis": { 
+      "genesis": {
         "genesis_state_timestamp": "${timestamp()}",
         "k": 225, 
         "delta": 1
@@ -51,8 +62,8 @@ module "testnet_east" {
     }
   EOT
 
-  seed_zone = "us-east1-b"
-  seed_region = "us-east1"
+  seed_zone = local.seed_zone
+  seed_region = local.seed_region
 
   log_level              = "Trace"
   log_txn_pool_gossip    = true
