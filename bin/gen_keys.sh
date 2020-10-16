22f82d57a70203f7b556e8d0dfb120e02f0fb413
Andrew Trainor <andrew@o1labs.org>
2020-10-16 18:30:03 -0400

    coda-network-integration Use the new keypath

--- bin/gen_keys.sh
+++ bin/gen_keys.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export CODA_PRIVKEY_PASS="naughty blue work"
+export CODA_PRIVKEY_PASS="naughty blue worm"
 for i in {1..251}
 do
-  coda advanced generate-keypair -privkey-path /usr/local/var/coda-network/keypairs/
+  coda advanced generate-keypair -privkey-path ./keys/keypairs/
   echo "Generated $i privkey"
 done
