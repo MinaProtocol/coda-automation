72fadca10efd247b7f2a99de31c6d27670e2e6a6
Andrew Trainor <andrew@o1labs.org>
2020-10-16 02:33:06 -0400

    coda-network-integration Move changes from integrate-coda-network-with-autodeploy

--- scripts/auto-deploy.sh
+++ scripts/auto-deploy.sh
@@ -4,7 +4,6 @@ set -e
 
 TESTNET="$1"
 CLUSTER="gke_o1labs-192920_us-east1_coda-infra-east"
-KEYSDIR=${2:-"./scripts/online_whale_keys"}
 
 docker_tag_exists() {
     IMAGE=$(echo $1 | awk -F: '{ print $1 }')
@@ -26,16 +25,14 @@ fi
 
 
 terraform_dir="terraform/testnets/$TESTNET"
-
-# TODO: add this check back, with compatibility for docker sha256 digests
-#image=$(sed -n 's|.*"\(0/coda-daemon:[^"]*\)"|\1|p' "$terraform_dir/main.tf")
-#image=$(echo "${image}" | head -1)
-#echo "WAITING FOR IMAGE TO APPEAR IN DOCKER REGISTRY"
-#for i in $(seq 60); do
-#  docker_tag_exists "$image" && break
-#  [ "$i" != 30 ] || (echo "expected image never appeared in docker registry" && exit 1)
-#  sleep 10
-#done
+image=$(sed -n 's|.*"\(0/coda-daemon:[^"]*\)"|\1|p' "$terraform_dir/main.tf")
+image=$(echo "${image}" | head -1)
+echo "WAITING FOR IMAGE TO APPEAR IN DOCKER REGISTRY"
+for i in $(seq 60); do
+  docker_tag_exists "$image" && break
+  [ "$i" != 30 ] || (echo "expected image never appeared in docker registry" && exit 1)
+  sleep 10
+done
 
 cd $terraform_dir
 echo 'RUNNING TERRAFORM'
@@ -44,33 +41,30 @@ terraform apply -auto-approve
 cd -
 
 echo 'UPLOADING KEYS'
+
 python3 scripts/testnet-keys.py k8s "upload-online-whale-keys" \
   --namespace "$TESTNET" \
   --cluster "$CLUSTER" \
-  --key-dir "$KEYSDIR"
+  --key-dir "keys/$TESTNET-online-whale-keys"
   
 python3 scripts/testnet-keys.py k8s "upload-online-fish-keys" \
   --namespace "$TESTNET" \
   --cluster "$CLUSTER" \
-  --count "$(echo scripts/online_fish_keys/*.pub | wc -w)"
+  --key-dir "keys/$TESTNET-online-fish-keys" \
+  --count "$(echo keys/$TESTNET-online-fish-keys/*.pub | wc -w)"
+
 python3 scripts/testnet-keys.py k8s "upload-service-keys" \
   --namespace "$TESTNET" \
-  --cluster "$CLUSTER"
+  --cluster "$CLUSTER" \
+  --key-dir "keys/$TESTNET-online-service-keys"
+
 if [ -e scripts/o1-discord-api-key ]; then
   kubectl create secret generic o1-discord-api-key \
     "--cluster=$CLUSTER" \
     "--namespace=$TESTNET" \
-    "--from-file=o1discord=scripts/o1-discord-api-key"
+    "--from-file=o1discord=keys/o1-discord-api-key"
 else
   echo '*** NOT UPLOADING DISCORD API KEY (required when running with bots sidecar)'
-fi
-if [ -e scripts/o1-google-cloud-storage-api-key.json ]; then
-  kubectl create secret generic o1-google-cloud-storage-api-key \
-    "--cluster=$CLUSTER" \
-    "--namespace=$TESTNET" \
-    "--from-file=o1google=scripts/o1-google-cloud-storage-api-key.json"
-else
-  echo '*** NOT UPLOADING GOOGLE CLOUD STORAGE API KEY (required when running with points sidecar)'
 fi
 
 echo 'DEPLOYMENT COMPLETED'
