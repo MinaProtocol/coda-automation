e2faf8b76973f33f28ccc900c19c5fc7887a37f1
Andrew Trainor <andrew@o1labs.org>
2020-10-16 22:24:55 -0400

    coda-network-integration Update generate-keys-and-ledger to consistently produce a ledger

--- scripts/generate-keys-and-ledger.sh
+++ scripts/generate-keys-and-ledger.sh
@@ -3,37 +3,58 @@
 TESTNET="${1:-pickles-public}"
 COMMUNITY_SIZE="${2:-10}"
 
-mkdir ./keys/keysets && mkdir ./keys/keypairs && mkdir ./keys/genesis
+mkdir ./keys/keysets
+mkdir ./keys/keypairs
+rm -rf ./keys/genesis && mkdir ./keys/genesis
 
-./bin/coda-network keyset create --count 10 --name ${TESTNET}_offline-whales
-./bin/coda-network keyset create --count 10 --name ${TESTNET}_online-whales
+for keyset in online-whales offline-whales online-fish offline-fish; do
+  [[ -s "keys/keysets/${TESTNET}_${keyset}" ]] || coda-network keyset create --count 10 --name "${TESTNET}_${keyset}"
+done
 
-./bin/coda-network keyset create --count $COMMUNITY_SIZE --name ${TESTNET}_offline-fish
-./bin/coda-network keyset create --count $COMMUNITY_SIZE --name ${TESTNET}_online-fish
+[[ -s "keys/keysets/${TESTNET}_online-service-keys" ]] || coda-network keyset create --count 2 --name ${TESTNET}_online-service-keys
 
-./bin/coda-network keyset create --count 2 --name ${TESTNET}_online-service-keys
 
+if [[ -s "keys/testnet-keys/${TESTNET}_online-whale-keyfiles/online_whale_account_1.pub" ]]; then
+echo "using existing keys"
+else
+  # Recreate + replace the online whale keys with ones we can put in secrets
+  sed -i 's/"publicKey":"[^"]*"/"publicKey":"PLACEHOLDER"/g' keys/keysets/${TESTNET}_online-whales
+  python3 ./scripts/testnet-keys.py keys generate-online-whale-keys --count 10 --output-dir $(pwd)/keys/testnet-keys/${TESTNET}_online-whale-keyfiles
+fi
+
+for f in keys/testnet-keys/${TESTNET}_online-whale-keyfiles/*.pub; do
+  echo "s/PLACEHOLDER/$(cat $f)/"
+  sed -i "s/PLACEHOLDER/$(cat $f)/" keys/keysets/${TESTNET}_online-whales
+done
+cat keys/keysets/${TESTNET}_online-whales
+
+# Generate the ledger
+echo
 echo "-- Generated the following keypairs and keysets for use in ./bin/coda-network genesis --"
 ls -R ./keys
 
-#./bin/coda-network genesis <<KEYSETS
-#${TESTNET}_offline-fish
-#1000
-#${TESTNET}_online-fish
-#y
-#${TESTNET}_online-fish
-#10
-#${TESTNET}_online-fish
-#y
-#${TESTNET}_offline-whales
-#1000000
-#${TESTNET}_online-whales
-#y
-#${TESTNET}_online-service-keys
-#1000000
-#${TESTNET}_online-service-keys
-#n
-#KEYSETS
+echo "ENTER THE FOLLOWING AT THE PROMPTS IN ORDER:"
 
+cat <<KEYSETS
+${TESTNET}_offline-fish
+60000
+${TESTNET}_online-fish
+y
+${TESTNET}_online-fish
+5000
+${TESTNET}_online-fish
+y
+${TESTNET}_offline-whales
+39000
+${TESTNET}_online-whales
+y
+${TESTNET}_online-service-keys
+39000
+${TESTNET}_online-service-keys
+n
+KEYSETS
 
-cat genesis_ledger.json.format | jq '[.[] | . + { sk: null, delegate: .delegate, balance: (.balance + ".000000000") }]' | cat > outfile.json << EOF
+coda-network genesis
+
+# Fix the ledger format for ease of use
+cat ./keys/genesis/* | jq '[.[] | . + { sk: null, delegate: .delegate, balance: (.balance + ".000000000") }]' | cat > terraform/testnets/${TESTNET}/genesis_ledger.json
