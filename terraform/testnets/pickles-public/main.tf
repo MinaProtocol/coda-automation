5234f27965e619a8f4f81602f33a80573fbbf5c7
Andrew Trainor <andrew@o1labs.org>
2020-10-16 23:25:46 -0400

    coda-network-integration Use more free ports

--- terraform/testnets/pickles-public/main.tf
+++ terraform/testnets/pickles-public/main.tf
@@ -82,7 +82,7 @@ module "testnet_east" {
   log_received_blocks    = true
 
   block_producer_key_pass = "naughty blue worm"
-  block_producer_starting_host_port = 10001
+  block_producer_starting_host_port = 10501
 
   block_producer_configs = concat(
     [
@@ -112,7 +112,7 @@ module "testnet_east" {
   snark_worker_replicas = 10
   snark_worker_fee      = "0.025"
   snark_worker_public_key = "B62qk4nuKn2U5kb4dnZiUwXeRNtP1LncekdAKddnd1Ze8cWZnjWpmMU"
-  snark_worker_host_port = 10400
+  snark_worker_host_port = 10401
 
   agent_min_fee = "0.06"
   agent_max_fee = "0.1"
