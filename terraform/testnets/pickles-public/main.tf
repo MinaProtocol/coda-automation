a494cce664eedb477fbbbd1913ae8faf8e773a93
Evan Shapiro <evanshapiro@MacBook-Pro-2.local>
2020-10-24 14:40:48 -0700

    update image tags

--- terraform/testnets/pickles-public/main.tf
+++ terraform/testnets/pickles-public/main.tf
@@ -23,8 +23,8 @@ provider "google" {
 locals {
   testnet_name = "pickles-public"
   # 0.0.16-beta7-feature-mainnet-parameter-test 0fdcc28
-  coda_image = "gcr.io/o1labs-192920/coda-daemon:0.0.16-beta7-develop-2847b2c"
-  coda_archive_image = "gcr.io/o1labs-192920/coda-archive:0.0.16-beta7-develop-2847b2c"
+  coda_image = "gcr.io/o1labs-192920/coda-daemon:0.0.16-beta7-qa-net-10-24-2020-af09fb6"
+  coda_archive_image = "gcr.io/o1labs-192920/coda-archive:0.0.16-beta7-qa-net-10-24-2020-af09fb6"
   seed_region = "us-east1"
   seed_zone = "us-east1-b"
   seed_discovery_keypairs = [
