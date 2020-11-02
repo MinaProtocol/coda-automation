c32d3e65d73061ee1ad53a75a3a79791fcea5f11
Andrew Trainor <andrew@o1labs.org>
2020-11-02 12:45:38 -0500

    coda-network-integration Remove unused script

--- bin/gen_keys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export CODA_PRIVKEY_PASS="naughty blue worm"
-for i in {1..251}
-do
-  coda advanced generate-keypair -privkey-path ./keys/keypairs/
-  echo "Generated $i privkey"
-done
