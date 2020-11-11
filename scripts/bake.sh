781f5f5bc7edeaa852a59cab6911e26e407dc23d
Aryeh Alexander <aryeh.alexander@gmail.com>
2020-11-11 18:24:37 -0500

    Add cloud build to bake script

--- scripts/bake.sh
+++ scripts/bake.sh
@@ -5,9 +5,10 @@ TESTNET=pickles-public
 DOCKER_TAG=0.0.16-beta7-develop
 AUTOMATION_PATHSPEC=$(git log master -1 --pretty=format:%H)
 CONFIG_FILE=/root/daemon.json
+CLOUD=false
 
 # arguments are
-# --docker-tag=, --testnet=, --config-file=, and --automation-commit=
+# --docker-tag=, --testnet=, --config-file=, --automation-commit=, and --cloud=
 while [ $# -gt 0 ]; do
   case "$1" in
     --testnet=*)
@@ -21,6 +22,9 @@ while [ $# -gt 0 ]; do
       ;;
     --config-file=*)
       CONFIG_FILE="${1#*=}"
+      ;;
+    --cloud=*)
+      CLOUD="${1#*=}"
       ;;
   esac
   shift
@@ -39,6 +43,17 @@ gcr_baked_tag="gcr.io/o1labs-192920/coda-daemon-baked:${DOCKER_TAG}-${TESTNET}-$
 docker_tag_exists() {
   curl --silent -f -lSL "https://index.docker.io/v1/repositories/codaprotocol/coda-daemon/tags/${DOCKER_TAG}" > /dev/null
 }
+
+if [[ $CLOUD == true ]]
+then
+  echo Building $gcr_baked_tag in the cloud
+
+  cd ./bake
+  gcloud builds submit --config=cloudbuild.yaml \
+  --substitutions=_BAKE_VERSION="$DOCKER_TAG",_COMMIT_HASH="$AUTOMATION_PATHSPEC",_TESTNET_NAME="$TESTNET",_CONFIG_FILE="$CONFIG_FILE",_GCR_BAKED_TAG="$gcr_baked_tag" .
+
+  exit 0
+fi
 
 for i in $(seq 60); do
   docker_tag_exists && break
