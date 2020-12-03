f82ce6f9ff195d60eaf9db26503e592eb3dcceee
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-12-03 14:11:14 -0500

    update testnet role Helm chart versions

--- terraform/modules/kubernetes/testnet/helm.tf
+++ terraform/modules/kubernetes/testnet/helm.tf
@@ -146,7 +146,7 @@ resource "helm_release" "seed" {
   name        = "${var.testnet_name}-seed"
   repository  = local.use_local_charts ? "" : local.mina_helm_repo
   chart       = local.use_local_charts ? "../../../helm/seed-node" : "seed-node"
-  version     = "0.2.2"
+  version     = "0.3.0"
   namespace   = kubernetes_namespace.testnet_namespace.metadata[0].name
   values = [
     yamlencode(local.seed_vars)
@@ -168,7 +168,7 @@ resource "helm_release" "block_producers" {
   name        = "${var.testnet_name}-block-producers"
   repository  = local.use_local_charts ? "" : local.mina_helm_repo
   chart       = local.use_local_charts ? "../../../helm/block-producer" : "block-producer"
-  version     = "0.2.2"
+  version     = "0.3.0"
   namespace   = kubernetes_namespace.testnet_namespace.metadata[0].name
   values = [
     yamlencode(local.block_producer_vars)
@@ -184,7 +184,7 @@ resource "helm_release" "snark_workers" {
   name        = "${var.testnet_name}-snark-worker"
   repository  = local.use_local_charts ? "" : local.mina_helm_repo
   chart       = local.use_local_charts ? "../../../helm/snark-worker" : "snark-worker"
-  version     = "0.2.2"
+  version     = "0.3.0"
   namespace   = kubernetes_namespace.testnet_namespace.metadata[0].name
   values = [
     yamlencode(local.snark_worker_vars)
@@ -202,7 +202,7 @@ resource "helm_release" "archive_node" {
   name       = "${var.testnet_name}-archive-node"
   repository  = local.use_local_charts ? "" : local.mina_helm_repo
   chart       = local.use_local_charts ? "../../../helm/archive-node" : "archive-node"
-  version    = "0.2.2"
+  version    = "0.3.0"
   namespace  = kubernetes_namespace.testnet_namespace.metadata[0].name
   values     = [
     yamlencode(local.archive_node_vars)
