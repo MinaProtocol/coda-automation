ff34613555ac26a7dc04d62ef9c3bd00e365a519
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-12-02 22:29:21 -0500

    more ci updates

--- terraform/modules/kubernetes/testnet/testnet-artifacts.tf
+++ terraform/modules/kubernetes/testnet/testnet-artifacts.tf
@@ -6,14 +6,14 @@ resource "null_resource" "block_producer_key_generation" {
 
 resource "null_resource" "prepare_keys_for_deployment" {
   provisioner "local-exec" {
-      command = "mv ../../../keys ../../../terraform/testnets/${var.testnet_name}/keys"
+      command = "mv ../../../keys ../../../terraform/testnets/${var.testnet_name} && sudo chmod -R a+rwX ../../../terraform/testnets/${var.testnet_name}/keys"
   }
   depends_on  = [kubernetes_namespace.testnet_namespace, null_resource.block_producer_key_generation]
 }
 
 resource "null_resource" "block_producer_whalekey_uploads" {
   provisioner "local-exec" {
-      command = "python3 ../../../scripts/testnet-keys.py k8s upload-online-whale-keys --namespace ${var.testnet_name} --cluster ${var.cluster_name} --key-dir ../../../terraform/testnets/${var.testnet_name}/keys/testnet-keys/${var.testnet_name}_online-whale-keyfiles"
+      command = "python3 ../../../scripts/testnet-keys.py k8s upload-online-whale-keys --namespace ${var.testnet_name} --cluster gke_o1labs-192920_${var.cluster_region}_${var.cluster_name} --key-dir ../../../terraform/testnets/${var.testnet_name}/keys/testnet-keys/${var.testnet_name}_online-whale-keyfiles"
   }
   depends_on  = [
     kubernetes_namespace.testnet_namespace,
@@ -24,7 +24,7 @@ resource "null_resource" "block_producer_whalekey_uploads" {
 
 resource "null_resource" "block_producer_fishkey_uploads" {
   provisioner "local-exec" {
-      command = "python3 ../../../scripts/testnet-keys.py k8s upload-online-fish-keys --namespace ${var.testnet_name} --cluster ${var.cluster_name} --key-dir ../../../terraform/testnets/${var.testnet_name}/keys/testnet-keys/${var.testnet_name}_online-fish-keyfiles"
+      command = "python3 ../../../scripts/testnet-keys.py k8s upload-online-fish-keys --namespace ${var.testnet_name} --cluster gke_o1labs-192920_${var.cluster_region}_${var.cluster_name}  --key-dir ../../../terraform/testnets/${var.testnet_name}/keys/testnet-keys/${var.testnet_name}_online-fish-keyfiles"
   }
   depends_on  = [
     kubernetes_namespace.testnet_namespace,
