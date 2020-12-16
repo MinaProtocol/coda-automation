543db7e11b8cf3e769436540be8d8f9411846dd1
Aryeh Alexander <aryeh.alexander@gmail.com>
2020-12-15 20:14:09 -0500

    Remove comments so ledger won't be regenerated

--- scripts/generate-keys-and-ledger.sh
+++ scripts/generate-keys-and-ledger.sh
@@ -191,11 +191,10 @@ generate_keyset_from_file "o1-keys.txt" "online-o1" "employee"
 
 # GENESIS
 
-# CI=1
-# if [[ !${CI} && -s "terraform/testnets/${TESTNET}/genesis_ledger.json" ]] ; then
-#   echo "-- genesis_ledger.json already exists for this testnet, refusing to overwrite. Delete \'terraform/testnets/${TESTNET}/genesis_ledger.json\' to force re-creation."
-#   exit
-# fi
+if [[ -s "terraform/testnets/${TESTNET}/genesis_ledger.json" ]] ; then
+  echo "-- genesis_ledger.json already exists for this testnet, refusing to overwrite. Delete \'terraform/testnets/${TESTNET}/genesis_ledger.json\' to force re-creation."
+  exit
+fi
 
 #if $COMMUNITY_ENABLED ; then 
 #    echo "-- Creating genesis ledger with 'coda-network genesis' --"
