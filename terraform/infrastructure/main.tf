319ae1e90f438a3d1e5807b18537f659dab8bd41
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-12-16 13:32:36 -0500

    add per region storage classes for both "Delete" + "Retain" reclaim policies

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -13,6 +13,11 @@ locals {
   gcp_project = "o1labs-192920"
   num_nodes_per_zone = 1
   node_type             = "n1-standard-16"
+
+  storage_reclaim_policies = [
+    "Delete",
+    "Retain"
+  ]
 }
 
 data "google_client_config" "current" {}
