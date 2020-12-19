bbe4a217c006afe3d0dcd84412fca7db8c6feb31
Andrew Trainor <andrew@o1labs.org>
2020-12-19 02:07:44 -0500

    aryeh/ci-net-baked-beans Update bots and add patch script

--- terraform/testnets/turbo-pickles/patch-nodes.sh
+++ terraform/testnets/turbo-pickles/patch-nodes.sh
@@ -2,18 +2,18 @@
 
 ready=""
 while [[ -z $ready ]]; do
-  ready=$(kubectl get pods -l app=whale-block-producer-2 | grep -P '\s+([1-9]+)\/\1\s+')
-  kubectl get pods -l app=whale-block-producer-2
+  ready=$(kubectl get pods -l app=fish-block-producer-1 | grep -P '\s+([1-9]+)\/\1\s+')
+  kubectl get pods -l app=fish-block-producer-1
   sleep 30
 done
 
 
-for whale in whale-block-producer-{3..15}; do
-  kubectl patch deploy/${whale} -p "$(cat patch.yaml)"
-  ready=""
-  while [[ -z $ready ]]; do
-    ready=$(kubectl get pods -l app=${whale} | grep -P '\s+([1-9]+)\/\1\s+')
-    kubectl get pods -l app=${whale}
-    sleep 30
-  done
-done
+#for whale in fish-block-producer-1 whale-block-producer-{1..15}; do
+#  kubectl patch deploy/${whale} -p "$(cat patch.yaml)"
+#  ready=""
+#  while [[ -z $ready ]]; do
+#    ready=$(kubectl get pods -l app=${whale} | grep -P '\s+([1-9]+)\/\1\s+')
+#    kubectl get pods -l app=${whale}
+#    sleep 30
+#  done
+#done
