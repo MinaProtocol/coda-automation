434e8aa4322684a5a1d46e0f45945a35ff10ea4b
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-12-23 11:48:42 -0500

    convert pagerduty integration key to aws secret and incorporate CR feedback

--- terraform/infrastructure/alerts.tf
+++ terraform/infrastructure/alerts.tf
@@ -6,13 +6,13 @@ locals {
             rules = [
                 {
                     alert   = "BlockProductionStopped"
-                    expr    = "increase(Coda_Transition_frontier_max_blocklength_observed{testnet=\"$testnet\"}[15m]) < 1"
-                    "for"   = "15m" 
+                    expr    = "increase(Coda_Transition_frontier_max_blocklength_observed{testnet=\"$testnet\"}[1h]) < 1"
+                    "for"   = "1h"
                     labels = {
                         severity = "critical"
                     }
                     annotations = {
-                        description = "Zero blocks have been produced on testnet {{ $labels.testnet }} for more than 15 minutes."
+                        description = "Zero blocks have been produced on testnet {{ $labels.testnet }} for more than 1 hour."
                         summary     = "{{ $labels.testnet }} block production is critically low"
                     }
                 }
@@ -22,11 +22,10 @@ locals {
   }
   pagerduty_receivers = [
     {
-        name = "pagerduty-primary"
+        name = "pagerduty-testnet-primary"
         pagerduty_configs = [
             {
-            # TODO: Store in AWS SecretManager 
-            service-key = "9aefac9667004bb78681288ba4161df0"
+                service-key = data.aws_secretsmanager_secret_version.pagerduty_testnet_primary_key_id.secret_string
             }
         ]
     }
