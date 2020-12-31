4ff26138e2121b820f23a3ae159ca68ddabb51bc
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-12-30 22:22:25 -0500

    add block gossip latency and zero snark fee alerts

--- terraform/infrastructure/alerts.tf
+++ terraform/infrastructure/alerts.tf
@@ -1,7 +1,5 @@
 locals {
-    production_filter = "testnet=~\"testworld|mainnet|qanet\""
-}
-locals {
+  production_filter = "testnet=~\"testworld|mainnet|qanet\""
   testnet_alerts = {
     groups = [
         {
@@ -15,7 +13,7 @@ locals {
                         severity = "critical"
                     }
                     annotations = {
-                        description = "Zero blocks have been produced on testnet {{ $labels.testnet }} for more than 1 hour."
+                        description = "Zero blocks have been produced on testnet {{ $labels.testnet }}."
                         summary     = "{{ $labels.testnet }} block production is critically low"
                     }
                 },
@@ -27,8 +25,37 @@ locals {
                         severity = "critical"
                     }
                     annotations = {
-                        description = "Critically low peer count on testnet {{ $labels.testnet }} for more than 1 hour."
+                        description = "Critically low peer count on testnet {{ $labels.testnet }}."
                         summary     = "{{ $labels.testnet }} avg. peer count is critically low"
+                    }
+                }
+            ]
+        },
+        {
+            name = "Warnings"
+            rules = [
+                {
+                    alert   = "HighBlockGossipLatency"
+                    expr    = "avg by (testnet) (max_over_time(Coda_Block_latency_gossip_time{${local.production_filter}}[1h])) > 200"
+                    for   = "1h"
+                    labels = {
+                        severity = "warning"
+                    }
+                    annotations = {
+                        description = "High block gossip latency (ms) within {{ $labels.testnet }} testnet."
+                        summary     = "{{ $labels.testnet }} block gossip latency is high"
+                    }
+                },
+                {
+                    alert   = "ZeroSnarkFeesObserved"
+                    expr    = "max by (testnet) (Coda_Snark_work_snark_fee_bucket{${local.production_filter}} == 0)"
+                    for   = "1h"
+                    labels = {
+                        severity = "warning"
+                    }
+                    annotations = {
+                        description = "Transactions containing zero SNARK fees observed on testnet {{ $labels.testnet }} for more than 1 hour."
+                        summary     = "{{ $labels.testnet }} SNARK work fees of zero Mina observed."
                     }
                 }
             ]
