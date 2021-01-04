70048fb570313e6aed216f16cd4adda72b2e9cdd
Ahmad Wilson <zer0ne.io.x@gmail.com>
2021-01-04 18:19:07 -0500

    update alert configs to make use of grafana cloud/cortex tool

--- terraform/infrastructure/alerts.tf
+++ terraform/infrastructure/alerts.tf
@@ -1,75 +1,109 @@
 locals {
-  production_filter = "testnet=~\"testworld|mainnet|qanet\""
-  testnet_alerts = {
-    groups = [
-        {
-            name = "Critical Alerts"
-            rules = [
-                {
-                    alert   = "BlockProductionStopped"
-                    expr    = "avg by (testnet) (increase(Coda_Transition_frontier_max_blocklength_observed{${local.production_filter}}[5m])) < 1"
-                    for   = "1h"
-                    labels = {
-                        severity = "critical"
-                    }
-                    annotations = {
-                        description = "Zero blocks have been produced on testnet {{ $labels.testnet }}."
-                        summary     = "{{ $labels.testnet }} block production is critically low"
-                    }
-                },
-                {
-                    alert   = "LowPeerCount"
-                    expr    = "avg by (testnet) (max_over_time(Coda_Network_peers{${local.production_filter}}[1h])) < 1"
-                    for   = "1h"
-                    labels = {
-                        severity = "critical"
-                    }
-                    annotations = {
-                        description = "Critically low peer count on testnet {{ $labels.testnet }}."
-                        summary     = "{{ $labels.testnet }} avg. peer count is critically low"
-                    }
-                }
-            ]
-        },
-        {
-            name = "Warnings"
-            rules = [
-                {
-                    alert   = "HighBlockGossipLatency"
-                    expr    = "avg by (testnet) (max_over_time(Coda_Block_latency_gossip_time{${local.production_filter}}[1h])) > 200"
-                    for   = "1h"
-                    labels = {
-                        severity = "warning"
-                    }
-                    annotations = {
-                        description = "High block gossip latency (ms) within {{ $labels.testnet }} testnet."
-                        summary     = "{{ $labels.testnet }} block gossip latency is high"
-                    }
-                },
-                {
-                    alert   = "ZeroSnarkFeesObserved"
-                    expr    = "max by (testnet) (Coda_Snark_work_snark_fee_bucket{${local.production_filter}} == 0)"
-                    for   = "1h"
-                    labels = {
-                        severity = "warning"
-                    }
-                    annotations = {
-                        description = "Transactions containing zero SNARK fees observed on testnet {{ $labels.testnet }} for more than 1 hour."
-                        summary     = "{{ $labels.testnet }} SNARK work fees of zero Mina observed."
-                    }
-                }
-            ]
-        }
-    ]
+  cortex_image = "grafana/cortextool:latest"
+}
+
+data "template_file" "testnet_alerts" {
+  template = "${file("${path.module}/templates/testnet_alerts.tpl")}"
+  vars = {
+    rule_filter = "{testnet=~\"testworld|.+\"}" # any non-empty testnet name + 'testworld'
+    alerting_timeframe = "1hr"
   }
-  pagerduty_receivers = [
-    {
-        name = "pagerduty-testnet-primary"
-        pagerduty_configs = [
-            {
-                service_key = "${data.aws_secretsmanager_secret_version.pagerduty_testnet_primary_key_id.secret_string}"
-            }
-        ]
-    }
+}
+
+data "template_file" "testnet_alert_receivers" {
+  template = "${file("${path.module}/templates/testnet_alert_receivers.tpl")}"
+  vars = {
+    pagerduty_receiver = "pagerduty-testnet-primary"
+    pagerduty_service_key = "${data.aws_secretsmanager_secret_version.pagerduty_testnet_primary_key.secret_string}"
+    pagerduty_alert_filter = "testworld"
+
+    discord_alert_receiver = "discord-alert-default"
+    discord_alert_webhook = "${data.aws_secretsmanager_secret_version.discord_testnet_alerts_webhook.secret_string}"
+  }
+}
+
+# Lint alerting config
+
+resource "docker_container" "lint_rules_config" {
+  name  = "cortex_lint"
+  image = local.cortex_image
+  command = [
+      "rules",
+      "lint",
+      "--rules-files=/alert_rules.yml"
   ]
+
+  upload = {
+      content = data.template_file.testnet_alerts.rendered
+      file ="/config/alert_rules.yml"
+  }
+}
+
+resource "docker_container" "check_rules_config" {
+  name  = "cortex_rules_check"
+  image = local.cortex_image
+  command = [
+    "rules",
+    "check",
+    "--rules-files=/config/alert_rules.yml",
+  ]
+
+  upload = {
+      content = data.template_file.testnet_alerts.rendered
+      file ="/config/alert_rules.yml"
+  }
+}
+
+resource "docker_container" "verify_alert_receivers" {
+  name  = "cortex_verify_receivers"
+  image = local.cortex_image
+  command = [
+    "alerts",
+    "verify",
+    "--address=${jsondecode(data.aws_secretsmanager_secret_version.alertmanager_api_auth.secret_string)["auth_url"]}",
+    "--id=${jsondecode(data.aws_secretsmanager_secret_version.alertmanager_api_auth.secret_string)["id"]}",
+    "--key=${jsondecode(data.aws_secretsmanager_secret_version.alertmanager_api_auth.secret_string)["password"]}"
+  ]
+}
+
+# Deploy alert updates
+
+resource "docker_container" "update_alert_rules" {
+  name  = "cortex_rules_update"
+  image = local.cortex_image
+  command = [
+    "rules",
+    "load",
+    "/config/alert_rules.yml",
+    "--address=${jsondecode(data.aws_secretsmanager_secret_version.current_prometheus_remote_write_config.secret_string)["remote_write_uri"]}",
+    "--id=${jsondecode(data.aws_secretsmanager_secret_version.current_prometheus_remote_write_config.secret_string)["remote_write_username"]}",
+    "--key=${jsondecode(data.aws_secretsmanager_secret_version.current_prometheus_remote_write_config.secret_string)["remote_write_password"]}"
+  ]
+
+  upload = {
+      content = data.template_file.testnet_alerts.rendered
+      file ="/config/alert_rules.yml"
+  }
+
+  depends_on  = [docker_container.check_rules_config, docker_container.lint_alert_config]
+}
+
+resource "docker_container" "update_alert_receivers" {
+  name  = "cortex_update_receivers"
+  image = local.cortex_image
+  command = [
+    "alertmanager",
+    "load",
+    "/config/pagerduty_alert_receivers.yml",
+    "--address=${jsondecode(data.aws_secretsmanager_secret_version.alertmanager_api_auth.secret_string)["auth_url"]}",
+    "--id=${jsondecode(data.aws_secretsmanager_secret_version.alertmanager_api_auth.secret_string)["id"]}",
+    "--key=${jsondecode(data.aws_secretsmanager_secret_version.alertmanager_api_auth.secret_string)["password"]}"
+  ]
+
+  upload = {
+      content = data.template_file.testnet_alert_receivers.rendered
+      file ="/config/pagerduty_alert_receivers.yml"
+  }
+
+  depends_on  = [docker_container.verify_alert_receivers]
 }
